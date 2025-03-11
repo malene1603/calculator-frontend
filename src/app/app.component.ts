99dad37866c8833f5cc13fd0042f8f2d8d2aa9a8
Malene1603 <143804625+Malene1603@users.noreply.github.com>
2025-03-11 13:15:45 +0100

    added frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calculator-frontend';
+  isCached: boolean = false;
+  number1: any;
+  number2: any;
+  result: string | undefined;
+  protected readonly history = history;
+  selectedOperation: string | undefined;
+  primeNumber: any;
+  factorialNumber: any;
+
+  calculate(add: string | undefined) {
+
+  }
+
+  checkPrime() {
+
+  }
+
+  calculateFactorial() {
+
+  }
 }
